0577599c37e7272ce9899e07f4f6e1c627708d25
Kemal Sarıalioğlu <kmlsarialioglu@gmail.com>
2021-10-20 09:56:45 +0300

    20 Ekim Begin

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
-console.log("Merhaba uzaylı. Biz dostuz...")
+﻿console.log("Merhaba Uzaylı. Ben dostum...")
 
-var dolarDun=9.2
-var dolarBugun=9.3
+let dolarDun=9.20
+let dolarBugun=9.30
+{
+    let dolarDun=9.10
+}
 
-//console.log(dolarDun)
+const euroDun=11.2
+// euroDun=11
+// console.log(euroDun)
 
-
-let konutKredileri=["Standart Konut kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-for (let index = 0; index < konutKredileri.length; index++) {
-    console.log("<li>" +konutKredileri[index]+"</li>") 
+console.log(dolarDun)
+//array leri çoğol isimlendir, array içinde array içinde array de dahil tüm formatlarda veri girişi yapabilirsin, 
+//camelCasing ilk kelime küçük harf
+//Pascalcasing ilk harfler büyük
+//let konutKrediliri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"["a",2]]
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
     
 }
+console.log("</ul>")
+
+{/* <ul>
+<li>Konut Kredisi</li>
+<li>Emlak Konut Kredisi</li>
+<li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri)
+
 let sayi1=10
 sayi1="kemal"
 let student={id:1,name:"kemal"}
